c2e57cfecfb00df3cdce622959a5e8cd5cf67682
小川大夢 <ogawahiromu@ogawataimunoMacBook-Air.local>
2026-03-11 14:51:24 +0900

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -28,6 +29,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-8M2WR7KZB5"
+        strategy="afterInteractive"
+      />
+      <Script id="ga" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8M2WR7KZB5');
+        `}
+      </Script>
       </body>
     </html>
   );
